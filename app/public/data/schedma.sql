cc83a30266ce38acd39b12181cbba6560e742c4a
jeffy1818 <jeffrey.yu1818@gmail.com>
2021-10-12 23:23:03 -0400

    Establish Cloud Connection
    Created MySQL, Connected AWS, Populated Database, Inserted HTML

---
+++ app/public/data/schedma.sql
@@ -0,0 +1,18 @@
+CREATE DATABASE IF NOT EXISTS finalproject;
+USE finalproject;
+
+DROP TABLE IF EXISTS referees;
+CREATE TABLE referees (
+	id int PRIMARY KEY,
+    firstName varchar(24),
+    lastName varchar(24),
+    age int,
+    grade int,
+    skill int
+);
+
+INSERT INTO referees (id, firstName, lastName, age, grade, skill) VALUES 
+(1111, 'Jeffrey', 'Yu', 25, 9, 80),
+(2222, 'Andrew', 'Wilderman', 22, 5, 90),
+(3333, 'Kevin', 'Alvarado', 23, 5, 90),
+(4444, 'Eshwar', 'Jayaprakash', 23, 1, 100);
---
+++ app/public/js/index.js
@@ -0,0 +1,31 @@
+const RefereeApp = {
+    data() {
+      return {
+        referees: []
+      }
+    },
+    computed: {},
+    methods: {
+        prettyData(d) {
+            return dayjs(d)
+            .format('D MMM YYYY')
+        },
+        fetchRefereeData() {
+            fetch('/api/referees/')
+            .then( response => response.json() )
+            .then( (responseJson) => {
+                console.log(responseJson);
+                this.referees = responseJson;
+            })
+            .catch( (err) => {
+                console.error(err);
+            })
+        }
+    },
+    created() {
+        this.fetchRefereeData();
+    }
+  
+  }
+  
+  Vue.createApp(RefereeApp).mount('#RefereeApp');
