1dbb9148cb43012b1864c25451a8efed29df02ff
kealva <alvkev336@gmail.com>
2021-11-01 13:21:25 -0400

    k_push

--- app/public/data/schedma.sql
+++ app/public/data/schedma.sql
@@ -16,3 +16,34 @@ INSERT INTO referees (refID, firstName, lastName, age, grade, skill) VALUES
 (2222, 'Andrew', 'Wilderman', 22, 5, 90),
 (3333, 'Kevin', 'Alvarado', 23, 5, 90),
 (4444, 'Eshwar', 'Jayaprakash', 23, 1, 100);
+
+DROP TABLE IF EXISTS games;
+CREATE TABLE games (
+	gameID int PRIMARY KEY,
+    stadium VARCHAR(24),
+    dy DATE,
+    tme TIME
+);
+
+INSERT INTO referees (refID, stadium, dy, tme) VALUES 
+(0001, 'Kuntz Stadium', 2021-11-04, 20:00:00)
+(0002, 'Kuntz Stadium', 2021-11-09, 19:00:00)
+(0003, 'Carroll Stadium', 2021-11-10, 18:30:00)
+(0004, 'Carroll Stadium', 2021-11-12, 17:30:00)
+(0005, 'Kuntz Stadium', 2021-11-13, 10:30:00);
+
+
+DROP TABLE IF EXISTS assignemnts;
+CREATE TABLE assignemnts (
+	gameID int PRIMARY KEY,
+    stadium VARCHAR(24),
+    dy DATE,
+    tme TIME
+);
+
+INSERT INTO assignemnts (refID, stadium, dy, tme) VALUES 
+(0001, 'Kuntz Stadium', 2021-11-04, 20:00:00)
+(0002, 'Kuntz Stadium', 2021-11-09, 19:00:00)
+(0003, 'Carroll Stadium', 2021-11-10, 18:30:00)
+(0004, 'Carroll Stadium', 2021-11-12, 17:30:00)
+(0005, 'Kuntz Stadium', 2021-11-13, 10:30:00);
