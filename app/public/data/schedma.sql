d6d4718fbb0f0cbdb89b8a18e2051841c00bb083
jeffy1818 <jeffrey.yu1818@gmail.com>
2021-11-07 00:35:15 -0400

    j_commit

--- app/public/data/schedma.sql
+++ app/public/data/schedma.sql
@@ -1,7 +1,10 @@
 CREATE DATABASE IF NOT EXISTS finalproject;
 USE finalproject;
 
+DROP TABLE IF EXISTS assignments;
+DROP TABLE IF EXISTS games;
 DROP TABLE IF EXISTS referees;
+
 CREATE TABLE referees (
 	refID int PRIMARY KEY AUTO_INCREMENT,
     firstName varchar(24),
@@ -17,7 +20,6 @@ INSERT INTO referees (refID, firstName, lastName, age, grade, skill) VALUES
 (03, 'Kevin', 'Alvarado', 23, 5, 90),
 (04, 'Eshwar', 'Jayaprakash', 23, 1, 100);
 
-DROP TABLE IF EXISTS games;
 CREATE TABLE games (
 	gameID int PRIMARY KEY AUTO_INCREMENT,
     stadium VARCHAR(24),
@@ -26,25 +28,28 @@ CREATE TABLE games (
 );
 
 INSERT INTO games (gameID, stadium, gameDate, gameTime) VALUES 
-(0001, 'Kuntz Stadium', 2021-11-04, '20:00:00'),
-(0002, 'Kuntz Stadium', 2021-11-09, '19:00:00'),
-(0003, 'Carroll Stadium', 2021-11-10, '18:30:00'),
-(0004, 'Carroll Stadium', 2021-11-12, '17:30:00'),
-(0005, 'Kuntz Stadium', 2021-11-13, '10:30:00');
+(01, 'Kuntz Stadium', 2021-11-04, '20:00:00'),
+(02, 'Kuntz Stadium', 2021-11-09, '19:00:00'),
+(03, 'Carroll Stadium', 2021-11-10, '18:30:00'),
+(04, 'Carroll Stadium', 2021-11-12, '17:30:00'),
+(05, 'Kuntz Stadium', 2021-11-13, '10:30:00');
 
-DROP TABLE IF EXISTS assignments;
+
 CREATE TABLE assignments (
 	assignmentID int PRIMARY KEY AUTO_INCREMENT,
-    gameID int,
-    stadium varchar(24),
-    gameDate date,
-    gameTime time, 
-    headRef varchar(24) default '--',
-    assistantRef_1 varchar(24) default '--',
-    assistantRef_2 varchar(24) default '--',
-    fourthOfficial varchar(24) default '--'
+    gameID int, 
+    refID int,
+    firstName varchar(24),
+    FOREIGN KEY (gameID) REFERENCES games(gameID),
+    FOREIGN KEY (refID) REFERENCES referees(refID)
 );
 
-INSERT INTO assignments (assignmentID, gameID, stadium, gameDate, gameTime, headRef, assistantRef_1, assistantRef_2, fourthOfficial) VALUES 
-(01, 01, 'Somewhere', 2021-11-04, '20:00:00', '--', '--', '--', '--');
+INSERT INTO assignments (assignmentID, gameID, refID, firstName) VALUES 
+(0001, 01, 01, 'Jeffrey Yu'); 
+
+SELECT * FROM assignments
+INNER JOIN referees ON assignments.refID = referees.refID;
+
+
+
 
